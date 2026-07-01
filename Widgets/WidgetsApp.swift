ac326a38c67f6f23c2b73426e67c21f9c3529f8b
Dom Montalto <dommontalto@icloud.com>
2026-07-01 08:17:16 +0800

    Initial Commit

---
+++ Widgets/WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsApp.swift
+//  Widgets
+//
+//  Created by Dom Montalto on 1/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
